b5ab3206821007a115fc5f9a6bca3de899cbfb33
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-01 18:49:33 -0400

    Modo estricto

--- main.js
+++ main.js
@@ -1,38 +1,19 @@
 /*
-    Comentarios en JS
-        - Son iguales que en cualquier lenguaje de programacion
-        - Comentarios de mas de una linea se encierran entre diagonal y asterisco
-        - Comentarios de 1 linea con doble diagonal 
+    - Para activar el modo estricto de JS: Se usa la sentencia 'use strict' al principio de cada fichero JS
+    - Este modo sirve para que el interpretador del navegador sea mas estricto con la sintacxis del lenguaje
+    por lo que si por ejemplo, no se define una variable correctamente se mostrara un error. 
+    - El lenguaje Javascript es debilmente tipado, por lo que si se define una variable asi:
+        pais = "españa"; - Entonces el codigo funcionara sin problemas
+    con el modo estricto esto no es asi, te mostrara un error, debe usarse la palabra reservada var.
+
+    - Es recomendable usar el modo estricto para todos los ficheros JS
 */
 
+//'use strict'
 
-/*
-    Variables en JS  
-        - Se definen con la palabra "var" segido del nombre de la varaible y su dato
-        - No tienen un tipo de dato asignado
-        - El tipo de dato se asigna dependiendo de que dato se almacene:
-            ejemplo: Si se almacena un string(osea dentro de comillas) el tipo de dato de la variable sera string
-        - El contenido de una variable se puede mostrar o usar tanto dentro de la consola como en el codigo JS
-        es decir que si se ejecuta "console.log(pais)" en la consola, esta mostrara el contenido  de esa variable.
-        - Separando con comas, se pueden encadenar varias variables
-        - Recordar que concatenar es añadir al final de una cadena de caracteres (string)
-*/
+var continente = "Latinoamerica"; // Esto funcionara en modo stricto.
+console.log(continente);
 
-var pais = "España"; // - Se asigna valor a una variable con "="
-var continente = "Europa"; // - Variable de tipo string 
-var año = 2020; // - Variable de tipo numerica (entero)
-
-año = 2121; // - Reasignacion de valor de una variable
-
-var pais_y_continente = pais+' - '+continente; // - Para concatenar se usa el signo "+", en este caso se concatena el contenido de una variable, un espacio y otra vairable
-
-console.log(pais); // - Salida de datos
-
-console.log(pais, continente, año); // - Salida de varios datos (mas de una variable)
-
-console.log(pais," - ", continente," - ", año); // - Tambien se puede concatenar asi... Usando comas
-
-console.log(pais_y_continente); // - Salida de datos 
-
-alert(pais_y_continente); // - Tambien se puede mostrar una variable en una alerta
+pais = "Venezuela"; // Esto no, pero si funcionara si no esta activado el modo stricto.
+console.log(pais);
 
