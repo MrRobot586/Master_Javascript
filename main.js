d32b1461016beeb64514f58d4c7863bd9ac0241d
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-01 20:46:45 -0400

    Constantes

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
 /*
-    Var Vs Let en JS
+    Constantes en JS
 
-    - 
+    - Una variable que no cambia en el transcurso del script
 */
 
 'use strict'
 
-//Ejemplo 1: Usando var se define una variable global cuyo valor cambia cuando se reasigna o se vuelve a declarar la variable
-var a = "Valor 1";
+var web = "https://victorroblesweb.es";// <-- Si se le puede cambiar el valor
+const ip = "198.88.0.12";// <-- No se le puede cambiar el valor
 
-console.log(a);// Tiene como salida "Valor 1"
+web = "https://vitorroblescursos.es";// <-- Reasignacion del valor
 
-if(true){
-    var a = "Valor 2";// Al usar var se redefine la variable con el mismo nombre. Por lo que de este punto en adelante, el valor sera el que se le asigne aqui
-    console.log(a);// Tiene como salida "Valor 2"
-}
+//ip = "198.87.0.12";// <-- Da como resultado un error
 
-console.log(a);// Tiene como salida "Valor 2"
+console.log(web);
 
-/* Al usar var para declarar la variable, esta se declara de forma gloval y su valor cambiara independientemente del bloque en el que se use */
-
-
-//Ejemplo 2: Usando let se define una variable a nivel de bloque. Es decir, esta solo tiene ese valor en ese bloque o ambito
-var b = "Valor 3";
-
-console.log(b);// tiene como salida "Valor 3"
-
-if(true){
-    let b = "Valor 4";// Al usar let, no se redefine la variable. Sino que se crea una nueva dentro de este bllque
-    console.log(b);// Tiene como salida "Valor 4"
-}
-
-console.log(b);// Tiene como salida "Valor 3"
-
+console.log(ip);// <-- Muestra el valor añadido en un inicio
