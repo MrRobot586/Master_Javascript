12b13a72d1fd3ce3300ae7c7ebe6cbea11f7f0a5
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-04 14:53:01 -0400

    Condicionales en JS

--- main.js
+++ main.js
@@ -1,17 +1,61 @@
 /*
-    Deteccion de tipo de datos en JS
-    - Esto se hace con la funcion typeof() y enviando como parametro la variable en cuestion
-    - Esta funcion puede usarse para comprobar datos antes de realizar operaciones.
+    Condicionales (if) en JS
+    
+    Si una condicion sucede se ejecuta un bloque de codigo
+
+    - Creo que ya sabes como funcionan los condicionales
 */
+
+//Nota: Basicamente el condicional if es tal como ya lo conoces en otros lenguajes de programacion
+
 
 'use strict'
 
-// Declaracion de variables - Para realizar pruebas
-var a = "Cadena de texto";
-var b = 1;
-var c = 1.5;
-var d = false;
+var edad1 = 30;
+var edad2 = 12;
 
-console.log(typeof(d)); // Esto muestra en la consola el tipo de dato detectado en la variable
+// Ejemplo 1:
 
-// Nota: Se pueden pasar como parametros a console.log otras funciones y este mostrara el resultado de la funcion
+if(edad1 > edad2){// Como edad1 es mayor que edad2 entonces se ejecutara esto
+    console.log("Edad1 es mayor que edad2!!");
+}else{// Sino esto
+    console.log("Edad1 es menor que edad2!!");
+}
+
+// Ejemplo 2:
+
+var edad = 18;
+var nombre = "Diego Oropeza";
+
+/*
+
+Operadores relacionales:
+
+
+>  - Mayor que
+<  - Menor
+<= - Menor o igual
+>= - Mayor o igual
+== - Igual que
+!= - Distinto de
+
+*/
+
+
+
+if(edad >= 18){ // Si edad >= a 18
+    console.log(nombre +" tiene "+ edad +" años, es mayor de edad.");
+
+    if(edad <= 33){
+        console.log("Todavia eres millenial.");
+    }else if(edad >= 70){
+        console.log("Eres anciano.");
+    }else{
+        console.log("Ya no eres millenial.");
+    }
+
+}else{// Sino
+    console.log(nombre +" tiene "+ edad +" años, es menor de edad.") ;
+}
+
+// Nota: Aca se denotan los codicionales dobles, anidados y los condicionales "elseif"
