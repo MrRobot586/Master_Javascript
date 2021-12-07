ba159467c385132bad9289bb8a7cc65928505a13
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-07 17:46:48 -0400

    Bloque 1 de ejercicios | Ejercicio 4

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
 /*
     Bloque 1 de ejersicios 
 
-    - Hacer un programa que muestre todos los numeros que hay entre 2 numeros que indique el usuario
+    - Hacer un programa que muestre todos los numeros impares que hay entre 2 numeros que indique el usuario
 */
 
 'use strict'
-// Variables
-var n1 = 0;
+// reciclamos el codigo anterior, el cual hara exactamente lo mismo, con una excepcion 
+var n1 = 3;
 var n2 = 0;
-
-// Do while para obtener datos filtrando por si son negativos, si no son numeros o si son iguales
 do{
 
-    // Obtencion de valores
     n1 = prompt("Digite un numero: ",0);
     n2 = prompt("Digite otro numero: ",0);
 
-    // Compobaciones
-    if(isNaN(n1) || isNaN(n2)){// Si no son nuemros || IsNaN comprueba si es que son numeros independientemente del tipo de dato
+    if(isNaN(n1) || isNaN(n2)){
         alert("Esos no son numeros!!");
-    }else{//Si son numeros
+    }else{
         
-        if(n1 < 0 || n2 < 0){// Si son negativos
+        if(n1 < 0 || n2 < 0){
             alert("Los numeros son negativos!!");
-        }else if(n1 == n2){// Si son iguales
+        }else if(n1 == n2){
             alert("Los numeros son iguales!!");
-        }else{// Si no a las anteriores condiciones
-            n1 = parseInt(n1);// Convertir los valores en numeros para cerrar el bucle
+        }else{
+            n1 = parseInt(n1);
             n2 = parseInt(n2);
         }
     }
 }while((isNaN(n1) || isNaN(n2)) || (n1 < 0 || n2 < 0) || (n1 == n2));
 
-// Si n1 es mayor que n2
-if(n1 > n2){// Mostrar desde n1 hasta n2 restando
+/* Los numeros pares, son cuyo reciduo al dividir por 2 es 0 entonces, los impares son lo contrario. */
+
+if(n1 > n2){
     document.write("<h2> De " + n1 + " a " + n2 + " hay:</h2> ");
     for(let i = n1; i >= n2; i--){
-        document.write( i + "<br>");
+        
+        if(i % 2 != 0){// Si es que la divicion de i (la iteracion) entre 2 es diferente de 0
+            document.write( i + "<br>");// Entonces es par y se imprime
+        }
+    
     }
 }
-// Si n2 es mayor
-if(n2 > n1){// Mostrar desde n1 hasta n2 sumando
-    document.write("<h2> De " + n1 + " a " + n2 + " hay:</h2> ");// Escribimos un h2 con el document.write
+
+if(n2 > n1){
+    document.write("<h2> De " + n1 + " a " + n2 + " hay:</h2> ");
     for(let i = n1; i <= n2; i++){
-        document.write( i + "<br>");// Usamos el document write para escribir los numeros
+
+        if(i % 2 != 0){// Basicamente lo mismo aca
+            document.write( i + "<br>");
+        }
+        
     }
 }
