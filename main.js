3f631ad7ae50eb5906421ebc3352fbb6d01367a2
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-07 17:34:27 -0400

    Bloque 1 de ejercicios | Ejercicio 3

--- main.js
+++ main.js
@@ -1,49 +1,48 @@
 /*
     Bloque 1 de ejersicios 
 
-    - Utilizando un bucle mostrar la media y la suma de los nuemros introducidos por el usuario hasta que se digite un numero negativo y en ese caso se mostraria el resultado
+    - Hacer un programa que muestre todos los numeros que hay entre 2 numeros que indique el usuario
 */
 
 'use strict'
+// Variables
+var n1 = 0;
+var n2 = 0;
 
-// Inicializacion de las variables
-var suma = 0;
-var contador = 0;
-var numero = 0;
-
-// Con un do while se iniciara la operacion y luego se evalua la condicion
+// Do while para obtener datos filtrando por si son negativos, si no son numeros o si son iguales
 do{
-    numero = prompt("Ingresa un numero para la suma: ");// Le metemos un valor a numero
 
-    if(numero != ''){// Si numero no esta vacio
-        if(isNaN(numero)){// Se comprueba de que efectivamente es un numero, si no lo es:
-            alert("El dato que ingresaste no es un numero... Intentalo de nuevo!");// Se muestra una alerta
-            numero = 0;// Se iguala a cero la variable
-        }else{// Si la variable es un numero
-            numero = parseInt(numero);// Se convierte a numero( Porque lo que devuelve prompt es un string)
+    // Obtencion de valores
+    n1 = prompt("Digite un numero: ",0);
+    n2 = prompt("Digite otro numero: ",0);
 
-            if(numero >= 0){// Se comprueba de que es positivo
-            
-                suma += numero;// Se efectua la suma
-                contador ++;// y se itera el contador
-                
-                // Con el contador y la suma total se ira sacando la media, dividiendo entre contador el total de valores sumados
-                alert("La media hasta ahora es: " + (suma / contador) + "\nLa sumatoria total va en: " + suma + "\n Se han sumado " + contador + " numeros hasta ahora...");
-            }else{// en caso de que numero no sea mayor que 0 (osea negarivo)
-                break;// Se rompera el ciclo
-            }
-        } 
-    }else{//en caso de que el numero este vacio
-        alert("El valor digitado esta vacio, intentalo de nuevo.");// Se muestra una alerta
+    // Compobaciones
+    if(isNaN(n1) || isNaN(n2)){// Si no son nuemros || IsNaN comprueba si es que son numeros independientemente del tipo de dato
+        alert("Esos no son numeros!!");
+    }else{//Si son numeros
+        
+        if(n1 < 0 || n2 < 0){// Si son negativos
+            alert("Los numeros son negativos!!");
+        }else if(n1 == n2){// Si son iguales
+            alert("Los numeros son iguales!!");
+        }else{// Si no a las anteriores condiciones
+            n1 = parseInt(n1);// Convertir los valores en numeros para cerrar el bucle
+            n2 = parseInt(n2);
+        }
     }
+}while((isNaN(n1) || isNaN(n2)) || (n1 < 0 || n2 < 0) || (n1 == n2));
 
-}while(numero >= 0 || numero == '');
-
-// Luego de terminar la operacion: 
-if(suma > 0 && contador > 0 ){
-    
-    console.log("La suma total de los numeros dio: " + suma + ", la media total dio: " + (suma / contador) + ", la cantidad de numeros sumados fue: " + contador);
-
-}else{
-    console.log("La suma no se completo exitosamente o fue cancelada.");
+// Si n1 es mayor que n2
+if(n1 > n2){// Mostrar desde n1 hasta n2 restando
+    document.write("<h2> De " + n1 + " a " + n2 + " hay:</h2> ");
+    for(let i = n1; i >= n2; i--){
+        document.write( i + "<br>");
+    }
+}
+// Si n2 es mayor
+if(n2 > n1){// Mostrar desde n1 hasta n2 sumando
+    document.write("<h2> De " + n1 + " a " + n2 + " hay:</h2> ");// Escribimos un h2 con el document.write
+    for(let i = n1; i <= n2; i++){
+        document.write( i + "<br>");// Usamos el document write para escribir los numeros
+    }
 }
