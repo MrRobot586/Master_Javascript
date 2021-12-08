a65e3c4119825c4b207d591c9c97ba691b4f2e58
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-08 18:52:36 -0400

    Bloque 1 de ejercicios | Ejercicio 5

--- main.js
+++ main.js
@@ -1,53 +1,33 @@
 /*
     Bloque 1 de ejersicios 
 
-    - Hacer un programa que muestre todos los numeros impares que hay entre 2 numeros que indique el usuario
+    - Programa que muestre todos los divisores de un nuemro introducido en un prompt
 */
 
 'use strict'
-// reciclamos el codigo anterior, el cual hara exactamente lo mismo, con una excepcion 
-var n1 = 3;
-var n2 = 0;
+
+var numero = 0;
+
+// Codigo reciclado del ejersicio anterior - para la logica al conseguir los datos
 do{
 
-    n1 = prompt("Digite un numero: ",0);
-    n2 = prompt("Digite otro numero: ",0);
+    numero = prompt("Digite un numero: ")
 
-    if(isNaN(n1) || isNaN(n2)){
-        alert("Esos no son numeros!!");
+    if(isNaN(numero)){
+        alert("Ese no es un numero!!");
     }else{
         
-        if(n1 < 0 || n2 < 0){
-            alert("Los numeros son negativos!!");
-        }else if(n1 == n2){
-            alert("Los numeros son iguales!!");
+        if(numero < 0){
+            alert("Ese numero es negativo!!");
         }else{
-            n1 = parseInt(n1);
-            n2 = parseInt(n2);
+           numero = parseInt(numero);
         }
     }
-}while((isNaN(n1) || isNaN(n2)) || (n1 < 0 || n2 < 0) || (n1 == n2));
+}while(isNaN(numero) || numero <= 0);
 
-/* Los numeros pares, son cuyo reciduo al dividir por 2 es 0 entonces, los impares son lo contrario. */
-
-if(n1 > n2){
-    document.write("<h2> De " + n1 + " a " + n2 + " hay:</h2> ");
-    for(let i = n1; i >= n2; i--){
-        
-        if(i % 2 != 0){// Si es que la divicion de i (la iteracion) entre 2 es diferente de 0
-            document.write( i + "<br>");// Entonces es par y se imprime
-        }
-    
-    }
-}
-
-if(n2 > n1){
-    document.write("<h2> De " + n1 + " a " + n2 + " hay:</h2> ");
-    for(let i = n1; i <= n2; i++){
-
-        if(i % 2 != 0){// Basicamente lo mismo aca
-            document.write( i + "<br>");
-        }
-        
+// Un numero es divisor de otro, es aquel que al dividirse entre el numero en cuestion su residuo da 0.
+for (let i = 1; i <= numero; i++){
+    if(numero % i == 0){// De esta forma filtramos a los numeros que son divisibles entre la variable "numero"
+        console.log("El numero: " + i + " es divisible entre " + numero);// Hacemos un console.log para la salida
     }
 }
