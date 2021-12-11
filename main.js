089b7589bea2b49e8780c10c2f93e9fb4be20a87
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-11 18:56:15 -0400

    Funciones dentro de otras - JS

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
 /*
-    Funciones | Parametros opcionales
-    - Existen parametros que ya estan inicalizados (tienen un valor predeterminado)
-    - Por lo que no son espesificamente necesarios a la hora de invocar la funcion
-    - Al contrario de los parametros normales que tienen que ser espesificados para que el codigo se ejcute bien
+    Funciones | Funciones dentro de otras
+    - Se pueden usar o invocar funciones dentro de otras, asi como tambien se pueden definir
 */
 
 'use strict'
 
-// FUncion que suma 2 numero (que recibira como parametros)
-function ejemplo(n1 = 0, n2 = 0){// Espesificamos un valor en la definicion de los parametros y la funcion
+// Se pueden tener varias funciones e invocarlas unas dentro de otras
+// Codigo reciclado
+function ejemplo(n1 = 0, n2 = 0){
     
-    if(n1 == 0 || n2 == 0){// Si los valores tienen el valor por defecto (0)
-        return "Espesifique los numeros para sumar";// Devuelve esto
-    }else{// Sino
-        return ("La suma dio como resultado: " + (n1 + n2));// Devuelve un mensaje con la suma
+    if(n1 == 0 || n2 == 0){
+        return "Espesifique los numeros para sumar";
+    }else{
+        return ("La suma dio como resultado: " + (n1 + n2));
     }
 
 }
 
-// Invocacion de la funcion sin parametros espesificados
-console.log(ejemplo());
+// En este caso existe la funcion "ejemplo" que recibe 2 parametros y devuelve un mensaje si no se espesifican parametros y el resultado si si los espesifican
 
-// Invocacion con parametros
-console.log(ejemplo(123, 44));
+function porconsola(){// Esta funcion dentro hace un console.log de el resultado (return) de la funcion ejemplo
+    console.log(ejemplo());// Esto imprimira lo que devuelva la funcion, en este caso un mensaje de error porque no hay parametros espesificacos
+}
 
-/*
+function porpantalla(){// Por otro lado esta funcion hace un document.write de la misma funcion
+    document.write(ejemplo(123, 44));// Imprimiendo un mensaje con el resultado de la suma, ya que se espesificaron los parametros
+}
 
-Nota: En este caso, creo que todos los parametros son opcionales asi tengan o no un valor asignado, ya que, 
-aunque no se le asigne un valor igualmente se ejecuta la funcion sin embargo no tendra un resultado coherente
-porque los valores no estan en los parametros, asi que un valor puede o no ser opcional dependiendo de si se toma
-en cuenta en la logica de la funcion. Como en este caso verificar si las variables valen 0.
+// Nota: La funcion ejemplo, esta siendo llamada en ambas funciones
+// Nota2: No olvidar ejecutar las funciones para comprobar
+// Nota3: El orden en el que se definan las funciones no afecta si una puede o no ser llamada dentro de otra, es decir, una funcion no debe ser definida antes de ser llamada dentro de otra. Bien puede ser definida despues de esta.
 
-*/
+// Invocacion de las funciones  
+porconsola();
+
+porpantalla();
