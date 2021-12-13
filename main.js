f77ec89e82582c8e7300bfc7a3bd38364ab4da95
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-13 16:36:23 -0400

    Metodos para busquedas - JS

--- main.js
+++ main.js
@@ -1,48 +1,58 @@
 /*
-    Funciones | Metodos para transformar textos
-    - Las variables initernamente se interpretan como objetos por lo que tienen metodos para manejar su contenido
-    - Existen muchos metodos para trasformar cadenas de texto y variables
-    - Para usar un metodo se usa el nombre de la variable (u objeto) seguido de un punto y el nombre del metodo
+    Funciones | Metodos para busquedas
+    - 
 */
 
 'use strict'
 
-var numero = 586;
-var texto = "Texto de relleno";
-var texto2 = "Texto de relleno 2";
+var texto = "Este es un texto random";
+var texto2 = "Aca continua el texto random, otra vez colocamos la palabra random.";
+var busqueda = " --- ";
+
+// Busca la palabra enviada como parametro al metodo y devuelve la posicion en la que se encuentra el primer caracter de la palabra en el string - en caso de no encontrar nada (-1) || Al encontrar la primera coincidencia ya para
+busqueda = texto.indexOf("texto");
+console.log(busqueda);
+
+// Hace lo mismo que el anterior
+busqueda = texto.search("texto");
+console.log(busqueda);
+
+// Busca la ultima coincidencia del string
+busqueda = texto2.lastIndexOf("random");
+console.log(busqueda);
+
+// Puede encontrar varias coincidencias y devolverlas como un objeto o array, con sus posiciones en el string en cuestion
+busqueda = texto.match("texto");
+// busqueda = texto.match(/texto/g); // Se usa asi para encontrar varias coincidencias
+console.log(busqueda);
+
+// Substrae un fragmento de un string, recibe como parametros la posicion en la que comienza la palabra y la longitud de la misma
+busqueda = texto.substr(11,5);// 11 es la posicion en la cual inicia la palabra y 5 la longitud (en esta caso la palabra texto)
+console.log(busqueda);
+
+// Busca y devuelve el caracter que se encuentre en la posicion enviada como parametro
+busqueda = texto2.charAt(55);
+console.log(busqueda);
+
+// Metodos de busqueda booleanos
+
+// Devuelve true si el string en cuestion inica con la palabra indicada
+busqueda = texto.startsWith("Este");
+console.log(busqueda);
+
+// Devuelve true si el string en cuestion termina en la palara o caracter indicado
+busqueda = texto.endsWith("random");
+console.log(busqueda);
+
+// Busca si existe la palabra en el string (en esta caso texto) - Devuelve true si es asi
+busqueda = texto.includes("texto");
+console.log(busqueda);
 
 /*
 
-Metodos para manejo de texto:
-    - tostring(): Convierte la variable en un string
-    - touppercase(): Convierte un texto en mayusculas
-    - tolowercase(): Convierte un texto en minusculas
-Otros metodos:
-    - lenght: En este caso no se usa los parentecis, pero sigue siendo un metodo. Devuelve la longitud de un string o del contenido de una variable
-    - concat(): Concatenar texto a una variable, enviadolo como parametro. 
+Nota:
+    - Cualquier metodo de busqueda, devolvera -1 en caso de que no encuentre una conincidencia
+    - Los metodos "Booleanos" devolveran false en caso de no haber coincidencias
+    - 
 */
 
-// Metodos para manejo de texto
-var dato = numero.toString();
-    dato = texto.toUpperCase();
-    dato = texto2.toLowerCase();
-
-console.log(dato);
-
-// Metodo leght
-var nombre = "Diego Oropeza";
-
-console.log(nombre.length);
-
-// Metodo concat()
-var finaltext = texto.concat(", otro texto de relleno...");
-
-console.log(finaltext);
-
-/*
-
-Nota: Los metodos no se aplican a las variables, es decir, si usas un metodo en una variable por ejemplo para convertir su contenido a mayusculas
-entonces, esa instancia sear en mayuscula, sin embargo, no cambiara el valor de la variable. Para cambiar el valor, lo recomendable es redefinir el valor de la variable
-por el valor que da como resultado el uso del metodo
-
-*/
