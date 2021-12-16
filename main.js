413c1d611d81654f8f15a94ec557e8db563a5c2b
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-16 18:53:45 -0400

    Operaciones con arrays - JS

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 /*
-    Arrays - Arreglos | Arrays multidimencionales
-    - Un arreglo multidimencional es aquel que contiene 2 o mas arreglos dentro
-    - Es decir, dentro de un indice se encuentra un arreglo y asi consecutivamente
+    Arrays - Arreglos | Operaciones con arrays
+    - Los strings pueden editarse, añadir nuevos elementos y eliminar elementos
 */
 
 'use strict'
 
 // Arrays de prueba 
 var lenguajes2021 = ["C++","C#","JS","Go"];
-var librerias2021 = ["Angular","Nodejs", "React"];
 
-// Asi se definiria un arreglo bidimencional, pero pueden ponerse mas de 2 arreglos dentro del mismo
-var programacion2021 = [lenguajes2021,librerias2021];
+// Añadir un elemento al final de un array | Se usa el metodo push: Que empuja al final un valor dado como parametro
+lenguajes2021.push("Python");
 
-// Asi se mostraria por consola un array multidimencional
-//console.log(programacion2021);
+// Eliminar el ultimo elemento de un array | Se usa el metodo pop: Quita el ultimo elemento de un array
+lenguajes2021.pop();
 
-// Asi se accede a un arreglo multidimencional - en el indice [0] se encutra el arreglo "lenguajes2021" y en el indice [1] de ese arreglo esta el dato "C#"
-//console.log(programacion2021[0][1]);
+// Eliminar un elemento en concreto |  Es mas metodico el proceso que solo un metodo, pero buscando el indice del elemento deseado y usando el metodo splice se puede hacer
+var indice_a_eliminar = lenguajes2021.indexOf("C++"); // Se busca y guarda el indice del elemento en cuestion
 
-// Asi se puede recorrer un arreglo multidimencional con el metodo foreach
-programacion2021.forEach((elemento,index)=>{// Primero se recorre la primera dimencion
-    console.log(index, "-----");// Se imprime el index 
+if(indice_a_eliminar > -1){// Si el indice es distinto de -1  o mayor a -1(que es lo que da el metodo indexof si no encuentra coincidencia)
+    lenguajes2021.splice(indice_a_eliminar,1);// El metodo splice elimina todos los elementos desde un indice indicado hasta n, es decir, desde el indice que es el primer parametro
+    // En este caso solo elimina un indice, porque solo se indica 1 como segundo parametro y se elimina desde el indice tantas veces como se indique como segundoparametro
+    // Es decir si el indice es 0, se elimina todos los siguientes elementos tantas veces como sea el segundo parametro (si es 2 se eliminan 2 elementos si es 1 se elimina 1)
+}
 
-    elemento.forEach((i)=>{// y se recorre el array que esta en el elemento en cuestion en el cual se esta iterando
-        console.log(i);// Se imprime
-    });
+// Convertir un array a texto separado por comas | Con el metodo join se genera un string a partir del array al que se le aplique el metodo, separando por comas sus elementos
+var string = lenguajes2021.join();
+
+console.log(string);// Salida de string resultante
+
+// Salida del array de pruebas
+lenguajes2021.forEach(elemento =>{
+    console.log(elemento);
 });
-
-/* Nota: Con el foreach, se recorre el array principal y dentro se recorren los que estan dentro de ese array. */
