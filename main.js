b8dbe0ee58e873b9c2c24c37dd5fbf63a4fc978e
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-16 19:09:32 -0400

    Ordenar arrays - JS

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 /*
-    Arrays - Arreglos | Convertir un string en un array
-    - Convertir una cadena de texto que tenga comas, algun separador o delimitador entre palabras
+    Arrays - Arreglos | Ordenar un array
+    - Metodos para ordenar un array
+    - Sort: Ordena de mayor a menor o de forma alfabetica un array
+    - Reverse: Invierte el orden de un array
 */
 
 'use strict'
 
-// String de prueba
-var lenguajes2021 = "C++, C#, JS, Go";// Este es un string inicial: Notar que cada palabra se separa por comas
+// Arrays de prueba
+var numeros = ["5","8","6","4"];
+var letras = ["C","G","H","A"];
 
-console.log(lenguajes2021);// Salida de prueba
+// Utilizando el metodo sort() - Se ordenan los arrays, claro este metodo si afecta al array directamente. No es necesario almacenar su resultado en una variable
+numeros.sort();
+letras.sort();
 
-// Se iguala una nueva variable a el resultado de el array aplicando el metodo split y enviando como parametro el separador de los que divide los elemtos
-var string_array = lenguajes2021.split(", ");// Recordar que un metodo no cambia el contenido de una variable
+// Salida de prueba
+console.log(numeros);
+console.log(letras);
 
-// Recoremos el nuevo array con un foreach para probar que funciono el metodo
-string_array.forEach(element => {
-    console.log(element);
-});
+// Con el metodo reverse se invierte el orden original del array
+numeros.reverse();
+
+// Salida de prueba
+console.log(numeros);
