5fc72ddb929c5e00c1945cffc81b4bdba0ef4491
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-28 16:33:39 -0400

    Validacion de un formularion con JS puro - JS

--- main.js
+++ main.js
@@ -1,66 +1,103 @@
 /*
-    Ejersicio completo DOM, Eventos y funciones
-        1. Formulario: nombre, apellidos y edad [x]
-        2. Boton para enviar el formulario: evento submit [x]
-        3. Mostrar datos por pantalla [x]
-        4. Boton que al darle click muestre los datos por pantalla [x]
+    Ejercicio Parte 2 | Validacion del formulario
 
 */
 
 'use strict'
 
-window.addEventListener('load', () =>{// Ejecucion del JS tras la carga de la pagina
-    
-    var form = document.getElementById("formulario");// Obtenemos el formulario HTML con el ID
-    
-    form.addEventListener('submit',(event) =>{// Añadimos un eventlistener y escuchamos el evento 'submit' | Se puede analizar este evento gracias al tipo de elemento
-        /*
-            El parametro event: Almacena el evento como tal, este puede servir para crear funcionalidades en torno a los datos que contenga el objeto dentro de dicha variable
-        */
-        event.preventDefault();// Este metodo, previene la carga de la pagina cuando se envie el formulario, esto aplicado al eventu submit
+window.addEventListener('load', () =>{
 
-        var datos = [];// Creamos un array vacio para los datos que se recibiran del formulario
+    // FUncion para comprobar los campos | Esta funcion recibe un array con los campos a validar y verifica que no este vacios
+    function comprobarcampos (campos){
+        if(campos.length > 0){// Verifica que la logitud del array sea mayor que 1
+            var invalidinputs = [];// Array para almacenar los campos vacios o "invalidos" | Almacenara los nombres
 
-        // Rellenamos dicho array con los 3 datos | Usando el metodo push para construir el array
-        datos.push(document.getElementById("nombre"));// Cabe destacar que se obtiene el elemento HTML como tal
-        datos.push(document.getElementById("apellido"));// No el valor, esto es para poder interactuar con sus diferentes atributos
-        datos.push(document.getElementById("edad"));// Como el valor y el nombre del campo en este caso, ya que se obtienen inputs desde el HTML
-
-        var datos_html = document.getElementById("datos_usr");// Obtenemos la seccion donde se colocaran los datos
-
-        // Creacion de titulo para la seccion de donde se mostraran los datos 
-        var h3 = document.createElement('h3');
-        h3.textContent = "Datos del usuario: ";
-
-        datos_html.prepend(h3); // Colocamos el h3 por ensima, como hijo del la seccion
-
-        // Un poco de logica en caso de que el formulario este vacio xd
-        if(document.getElementById('nombre').value == '' || document.getElementById('apellido').value == '' || document.getElementById('edad').value == ''){// Si alguno de los campos esta vacio
-            datos_html.append(document.createTextNode('[Vacio] - El formulario esta vacio o algun dato no esta lleno...'));// Mostrar este mensaje en vez de los datos..
-        }else{// Caso contrario | En caso de que si existan los datos
-            var lista = document.createElement('ul');// Creamos un elemento de lista desordenada
-        
-            for(let i in datos){// Usando un for in iteramos en el array de los datos
-                let li_content = document.createElement('li');// Creando items para la lista
-
-                let b = document.createElement('b');// Ademas de un elemento b para poner en negrita el titulo de cada elemento
-
-                b.textContent = datos[i].name + ": ";// Asignamos  el nombre de cada elemento dentro del b + un detalle que son los ": " para diferenciar el titulo del valor
-
-                //b.append(datos[i].name + ": ");// El codigo de arriba tambien se puede hacer con append de esta manera
-
-                li_content.append(b);// Asignamos como hijo el b (que contiene el nombre del elemento y el detalle de ": ") al item de la lista
-
-                li_content.append(datos[i].value);// Por ultimo, al item le asignamos el valor del campo obtenido en la iteracion actual (osea i)
-
-                lista.append(li_content);// Y a la lista se le añade el elemento
-
-                /* En resumen: Basicamente se construyo el item de la lista para despues asignarlo a la lista y se le fueron añadiendo elmentos a la lista en cada iteracion,
-                por lo que se construyo la lista en el bucle y el resultado se incrusto en la seccion correspondiente. */
+            for(let i = 0; i < campos.length; i ++){// For para recorrer el array
+                if(campos[i].value != ''){// Verificar si el campo en el que se itera esta vacio, si no es asi
+                    if(isNaN(campos[i].value)){// Comprobar si no es un numero
+                        // Quitar los espacios al inicio y final con trim
+                        let string = campos[i].value;
+                        string.trim();
+                        campos[i].value = string;
+                    }
+                }else{// En caso de que el campo este vacio
+                    invalidinputs.push(campos[i].name);// Se guarda su  nombre en el array
+                }
             }
 
-            datos_html.append(lista);// Ahora solo metemos esa lista en la seccion donde van os datos del usuario y ya sera visible
+            if(invalidinputs.length > 0){// Si la longitud del array que contiene los errores es mayor que cero | Significa que hubo campos vacios, por tanto se procede a alertar
+                
+                if(invalidinputs.length == 1){// Si es solo uno | Es decir solo existe una posicion en el array
+                    alert(`[Error] - El campo "${invalidinputs[0]}" esta vacio...`);// Se envia una alerta
+                }else{// Si existen mas
+                    let invalids = "";// Se arma un array con los nombres de los campos que estan vacios
+                    for(let i = 0; i < invalidinputs.length; i ++){
+                        invalids += invalidinputs[i] + ", ";
+                    }
+                    // Se alerta al usuaro usando el array para señalar los campos
+                    alert("[Error] - Existen campos vacios en el formulario...\nLos campos "+ invalids +" estan vacios...\nLlenalos antes de envaiar el formulario.");
+                }
+                // Se hace return false para que acabe la funcion
+                return false;
 
+            }else{// Caso contrario, los campos fueron validos y se hace el return del array para que se use 
+                return campos;
+            }
+
+            
+        }
+    }
+
+    // Codigo reciclado del anterior ejercicio | Un poco modificado
+    var form = document.getElementById("formulario");
+
+    var datos_html = document.getElementById("datos_usr");
+
+    datos_html.style.display = 'none';
+    
+    form.addEventListener('submit',(event) =>{
+
+        event.preventDefault();
+
+        var datos = [];
+
+        datos.push(document.getElementById("nombre"));
+        datos.push(document.getElementById("apellido"));
+        datos.push(document.getElementById("edad"));
+
+        datos_html.innerHTML = "";
+
+        if(datos = comprobarcampos(datos)){
+            /*
+                Se ejecuta la funcion en el condicional, igualando la el array de los datos a la misma y enviando como parametro el mismo array.
+                El resultado de esa ejecucion sera tomado en cuenta para el condicional, si devuelve true ejecutara el codigo
+                si es false, no. Excepto que no devolvera true, sino un array nuevo entonces, si devuelve algo que no es false se considera true y pasa el condicional
+            */
+            
+            var h3 = document.createElement('h3');
+            h3.textContent = "Datos del usuario: ";
+            datos_html.prepend(h3);
+            
+            var lista = document.createElement('ul');
+
+            for(let i in datos){
+                let li_content = document.createElement('li');
+
+                let b = document.createElement('b');
+
+                b.textContent = datos[i].name + ": ";
+
+                li_content.append(b);
+
+                li_content.append(datos[i].value);
+
+                lista.append(li_content);
+
+            }
+
+            datos_html.append(lista);
+
+            datos_html.style.display = 'block';
         }
     });
 
