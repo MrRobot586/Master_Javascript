a81b59f8a22515eb56b7a97b9d1506cbfffeeedf
Diego Oropeza <diegojoseoropezagmail.com>
2021-12-29 11:38:03 -0400

    JavaScript Object Notation - JS

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 /*
-    Operador this | Sirve para hacer referencia al elemento dentro de un eventlistener...
-        El operador this, hace referencia a un elemento al cual se le añade un ventlistener, es decir, seleccionas un elemento
-        y luego escuchas el evento click, dentro de la funcion de callback se puede usar this para hacer referencia al propio elemento
-        del cual estas escuchando el evento.
+    JSON - JavaScript Object Notation
+        - Sirve para declarar y utilizar objetos, que sirven como arrays asociativos, es decir, arrays cuyos indices son en erealidad referencias al contenido del indice como tal.
+        - Cada objeto json, tiene propiedades y metodos, las propiedades son definidas o declaradas y los metodos sirven para modificar alguna caracteristica o convertir el objeto.
+        - Para acceder a las propiedades de un objeto json se usa el nombre del elemento seguido de punto y la propiedad, ejemplo: "objeto.propiedad"
+        - Se pueden declarar objetos json dentro de otros o incluso objetos js como valores de algun indice de un array
 */
 
 'use strict'
 
 window.addEventListener('load',()=>{
 
-    var boton = document.getElementById('click');// Se obtiene el elemento
+    // De esta forma se define un objeto JSON
+    var pelicula = {// Nombre del objeto seguido de la asignacion que se hace entre llaves
+        titulo: 'Bantan Vs. Superman',// Cade elemento se define con el nombre del mismo + dos puntos y el valor
+        year: 2016,// Cada elemento se separa por comas
+        pais: 'Estados unidos'// Excepto el ultimo que no lleva comas
+    };// Por supuesto se cierran las llaves y se coloca  ";"
 
-    boton.addEventListener('click',function (){// Se le añade un eventlistener para escuchar el evento click
-        if(this.style.background == "teal"){// Un switch de color usando "this" - Si la subpropiedad, background la propiedad style del elemento "this" (osea boton) es "teal"...
-            this.style.background = "tomato";// Mediante this hacemos referefia al elemento "boton" que es el elemento del cual capturamos el evento y cambiamos el valor de la propiedad
-        }else{// Sino...
-            this.style.background = "teal";// Hacemos lo mismo pero con otro color
-        }
-        // Nota: El this solo funciona con funciones anonimas de callback (no funciona confunciones flecha) y obviamente se usa solo para eventos
-        // Nota2: El this funciona para hacer referencia al elemento del cual se escuha un evento en concreto, solo se puede hacer referencia con este operador dentro de la funcion de callback del eventlistener
-    });
+    console.log(pelicula);// Salida de prueba para ver el objeto con todas sus propiedades
+
+    pelicula.year = 2017;// Asi se puede reasignar el valor de una propiedad (solo se accede a ella y se le da otro valor)
+
+    console.log(pelicula.year);// Asi se accede al valor de una propiedad espesifica
+
+    // Asi se define un array con un objeto json dentro
+    var peliculas = [
+        {titulo: 'cuestion de tiempo', year: '2013', pais: 'Reino Unido'},// Cada elemento separado por coma, pero en este caso el elemento esta entre llaves y tiene distintas propiedades
+        pelicula
+    ];
+
+    console.log(peliculas);// Salida de prueba
+
+    console.log(peliculas[0].titulo);// Asi se accederia a un indice y luego a una propiedad de ese objeto (que esta dentro del indice)
+
+    for(let index in peliculas){// Asi se recorreria un array
+        console.log(peliculas[index].titulo + " - " + peliculas[index].year);// Aca se imprime en consola el titulo y año de cada elemento
+    }
+
 });
