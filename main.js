f498554b2149ffed16c2127a3b820f53447292d1
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-01 14:50:22 -0400

    peticiones ajax y fetch - JS

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
 /*
-    LocalStorage en JS | Ejercicio completo: Explicacion del video
-        Crear una lista de pelicuas en la que se puedan eliminar y crear elementos
-        (Utilizando, eventos y el localstorage)
+    Peticiones ajax y fectch (peticiones asincronas)
+        Las peticiones ajax sirven para obtener datos de un back-end, como una api o una base de datos.
+        Estas peticiones suelen tardar cierto tiempo en responder con los datos, es aqui donde entran las promesas en juego
+        pues con estas se puede ejecutar un codigo luego de que una peticion ajax (con fetch) haya resultado exitosa o en un error.
 */
 
 'use strict'
 
 window.addEventListener('load',function (){
 
-    if(localStorage != undefined){// Comprobamos de que el localstorage es compatible con el navegador
+    var users = [];// Creamos un array para almacenar los datos que recojeremos de la peticion ajax
+    
+    /*
+        Para realizar una peticion ajax usando fetch, se invoca a la funcion enviando como parametro la URL de la api,
+        esta a su vez devuelve una promesa, a la cual podemos acceder mediante la palabra reservada ".then", es decir,
+        una vez se termine la consulta la funcion fetch devolvera una promesa la cual porcesaremos en una funcion de callback
+        que se adjutara a la funcion ".then".
+
+        basicamente, invocamos a la funcion fetch, con ".then" ejecutamos una fucnion de callback usando como parametro lo que devuelva
+        la funcion fetch que es una promesa. De esta promesa sacaremos la data que recibimos en la peticion ajax.
+    */
+    fetch('https://reqres.in/api/users?page=1')// Invocamos la funcion fetch enviando como parametro la URL de la api a consultar
+        .then(data => data.json())// Con .then esperamos a que se ejecute la peticion y procesamos los datos en una funcion anonima (callback)
+        // En este caso, returnamos data aplicandole el metodo ".json()", que convierte la promesa en formato json
         
-        var form_p = document.getElementById("formpelis");// Obtenemos el formulario
+        /* // Este seria el ejemplo del codigo de arriba pero con una funcion anonima y mas completa que una de flecha, mas codigo pero mas claro.
+        .then(function (data){
+            return data.json();
+        })
+        */
+        
+        .then(data => {// Cuando se procese la informacion, encadenamos la siguiente operacion (es decir la funcion de callback), recordar que "data" el resultado del anterior proceso o funcion
+            users = data.data;// Igualamos users a data.data (que son los datos que buscamos en el JS)
 
-        form_p.addEventListener('submit', function (event){// Capturamos el evento submit
+            var ul = document.createElement('ul');// Creamos una lista
             
-            //event.preventDefault();// Prevenimos el default del evento, que es recargar la pagina
-
-            var titulo = document.querySelector('#p_titulo').value;// Obtenemos el valor del elemento con el id "p_titulo"
-            
-            if(titulo.length >= 1){// comprobamos si el texto cabturado no esta vacio
-                localStorage.setItem(titulo,titulo);// Y creamos un elemento en el localstorage con el nombre de ese texto
+            /* // Podemos recorrer los elementos del array (que fue lo que devolvio la peticion) con un for
+            for(let i in users){
+                console.log(users[i].id, users[i].email, users[i].first_name, users[i].last_name);
             }
+            */
 
-            // Mostrar los elementos
-            var ul = document.getElementById("peliculas_list");// obtenemos el elemento lista del body
+            // O tambien usar un metodo llamado "map", que recibe una funcion de callback para iterar en un bucle
+            users.map(function (user,i){// Esta funcion devuelve el elemento en el cual estamos iterando de un array, lo que la hace perfecta para iterar arrays de objetos (el i es opcional)
+                //console.log(user.id, user.email, user.first_name, user.last_name);// Como devuleve cada elemento, solo debemos acceder a las propiedades de cada objeto para obtener sus datos
+                // Ya con este codigo simplemente armamos la lista
+                var li = document.createElement('li');
+                li.textContent = user.id + " - " + user.first_name + " " + user.last_name;
+                ul.append(li);
+            });
 
-            for(let i in localStorage){// Recorremos los elementos del localstorage (si, se pueden recorrer los elementos del objeto localstorage con un for)
-                var li = document.createElement('li');// Creamos un item para añadir a la lista
-                
-                if(typeof(localStorage[i]) == 'string'){// Añadimos un elemento si es que la iteracion del localstorage es un string
-                    li.textContent = localStorage[i];// Entonces le asignamos el contenido de esa iteracion al item de lista
-                    ul.append(li);// Y el item a la lista
-                }
-            }
-            
+            // Mostrar la lista - O mas bien montarla en el body
+            document.body.append(ul);
+
         });
-
-        // Formulario para eliminar elementos
-        var form_p_d = document.getElementById("formpelis_del");// Obtenemos el formulario
-
-        form_p_d.addEventListener('submit', function (event){// Capturamos el evento submit
-            
-            //event.preventDefault();
-
-            var titulo = document.querySelector('#p_titulo_del').value;// Obtenemos el elemento a eliminar del formulario (ubicandolo con el id)
-            
-            if(titulo.length >= 1){// Si es un elemento valido lo eliminamos
-                localStorage.removeItem(titulo);// Removemos el elemento del localstorage
-            }
-            
-        });
-
-    }else{
-        console.log("LocalStorage no disponible en este navegador...");
-    }
 
 });
