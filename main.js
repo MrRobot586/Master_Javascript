47e899d0f43364cb33b0e696d6add5fd61486aed
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-05 10:47:45 -0400

    Seleccionar elementos por atributo Jquery - JS

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*
-    Jquery en JavaScript | Selectores de etiquetas   
+    Jquery en JavaScript | Selectores de atributos
+        - Para seleccionar un elemento por sus atributos se  hace uso de los corchetes "[]" 
 */
 
 
@@ -7,33 +8,14 @@
 
 $(document).ready(function () {
 
-    // Para seleccinar una etiqueta simplemente ponemos su nombre como parametro al invocar al jquery
-    var parrafos = $('p');// En este caso le damos su referencia a la variable parrafo
-    parrafos.css('cursor','pointer');// Ademas de darle un estilo (que el cursor sea un puntero al pasarlo ensima)
+    // En este caso seleccionamos a "google", por su atributo "href" y a "facebook" por su atributo "title"
+    var google = $('[href="https://www.google.com"]');
+    var facebook = $('[title="ir a facebook"]');
 
-    // Para el ejemplo, se usara el evento click para cambiar los estilos de cada parrafo segun su contenido
-    parrafos.click(function (){// Escuchando el evento click
-        let p = $(this).text();// Con el metodo "text()", obtenemos o editamos el texto que tenga esa etiqueta o elemento seleccionado, en este caso el del parrafo
+    // Salida de prueba
+    console.log(facebook, google);
 
-        switch (p.toLowerCase()) {// En este caso se usa el metodo "tolowercase", para que este todo el texto en minuscula y compararlo bien
-            // Editamos los estilos css segun sea el caso, usando this como selector, ya que es el elemento al que le damos click
-            case 'la bandera':
-                $(this).css('background','yellow')
-                       .css('color','gray')
-                       .css('text-align','center');
-                break;
-
-            case 'de':
-                $(this).css('background','blue')
-                       .css('color','white')
-                       .css('text-align','center');
-                break;
-            case 'venezuela':
-                $(this).css('background','red')
-                       .css('color','white')
-                       .css('text-align','center');
-                break;
-        }
-    });
-
+    // De igual forma a estos elementos seleccionados por atributos se le pueden modificar los estilos css o lo que sea con javascript
+    google.css('color','tomato');
+    facebook.css('color','teal');
 });
