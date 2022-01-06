e010bde08bc4ecc979ed9efa626dcf5ee4377885
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-06 18:48:04 -0400

    Posibilidades con Jquery, find y parent - JS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*
-    Jquery en JavaScript | Selectores de atributos
-        - Para seleccionar un elemento por sus atributos se  hace uso de los corchetes "[]" 
+    Jquery en JavaScript | find y parent
+        Curiosidades y otras posibilidades con Jquery
 */
 
 
@@ -8,14 +8,21 @@
 
 $(document).ready(function () {
 
-    // En este caso seleccionamos a "google", por su atributo "href" y a "facebook" por su atributo "title"
-    var google = $('[href="https://www.google.com"]');
-    var facebook = $('[title="ir a facebook"]');
+    // 1. Se pueden seleccionar varios tipos de elementos separandolos por coma los selectores | Con jquery
+    $('li, p').css('background','tomato') 
+              .css('color','white');
 
-    // Salida de prueba
-    console.log(facebook, google);
+    // 2. Tambien podemos encontrar un elemento dentro de otro (en caso de no saber donde se encuentra), esto con el metodo find
+    $('p').find('.span')// Enviando como parametro el selector del elemento a buscar - El metodo find, sirve para buscar dentro de un arbol de elementos muy extenso
+          .css('background','green');// En este caso encadenamos un css para cambiar el fondo del elemento encontrado
+    // Se pudo hacer $('p .span') y de igual forma se encuentra los elementos
 
-    // De igual forma a estos elementos seleccionados por atributos se le pueden modificar los estilos css o lo que sea con javascript
-    google.css('color','tomato');
-    facebook.css('color','teal');
+    // 3. Podemos seleccionar   el padre de un elemento usando el metodo "parent()", este metodo da como resultado el elemento HTML padre del elemento actualmente seleccionado
+    var span = $('p .span');// Ojo: Esto no cambia el contenido de una variable, que contenga un elemento html
+    console.log(span.text());// Salida de prueba, el elemento seleccinado sigue siendo el span dentro del parrafo
+    span.parent().css('color','yellow');// Al padre del span (osea el parrafo), le cambiamos el CSS
+
+    // 4. Eq() - Buscar un elemento en concreto dentro de una conjunto de elementos HTML, retornado por una seleccion
+    var lista = $('li');// En este caso seleccionamos todos los elemetos Li
+    lista.eq(0).css('background','blue');// y al primero de ellos (el 0), le cambiamos el bg a azul. Seleccionandolo con EQ()
 });
