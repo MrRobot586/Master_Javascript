1481bef029107f6c1a3025d2dd7d4ac33694bd39
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-07 21:07:05 -0400

    Evento hover en Jquery - JS

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
-    Jquery en JavaScript | Eventos mouseover y mouseout
+    Jquery en JavaScript | Evento hover
 */
 
 
@@ -7,19 +7,29 @@
 
 $(document).ready(function (){
 
-    // Con jquiery los eventos son mas faciles de capturar
+    // seleccionamos el elemento con jquery
     const caja = $('#caja');
 
-    // En este caso aplicamos el evento mouseover - Para esto aplicamos el metodo mouseover y le damos como parametro una funcion de calback en al que ejecutaremos el codigo al capturar el evento
-    caja.mouseover(function () { 
-        $(this).css('background','gray')// En este caso cambiamos el fondo y el texto
+    // Capturamos el evento hover: Que se activa cuando el mouse esta ensima del elemento y tambien cuando esta fuera.
+    // Es decir, captura tanto el mouseover como el mouseout en un solo evento | Se captura con el metodo "hover"
+    caja.hover(function(){// Se envian como parametros 2 funciones de callback de las cuales la primera es para mouseover y la segunda para mouseout
+        $(this).css('background','gray')
                .text(':(');
-    });
-
-    // Lo mismo poara el evento mouseout
-    caja.mouseout(function () { 
+    },function(){
         $(this).css('background','salmon')
                .text(':)');
     });
 
+    // Curiosamente tambien se pueden enviar 2 funciones ya hechas, pero sin los parentencis ni parametros en estas
+    caja.hover(feli,tiste);
+
+    function feli() {
+        $(this).css('background','salmon')
+               .text(':)');
+    }
+
+    function tiste() {
+        $(this).css('background','gray')
+               .text(':(');
+    }
 });
