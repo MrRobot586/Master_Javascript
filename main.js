5b97eb207919c9773148b758feefeb1101e187d4
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-07 20:40:38 -0400

    Eventos de mouse con jquery - JS

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
 /*
-    Jquery en JavaScript | find y parent
-        Curiosidades y otras posibilidades con Jquery
+    Jquery en JavaScript | Eventos mouseover y mouseout
 */
 
 
 'use strict'
 
-$(document).ready(function () {
+$(document).ready(function (){
 
-    // 1. Se pueden seleccionar varios tipos de elementos separandolos por coma los selectores | Con jquery
-    $('li, p').css('background','tomato') 
-              .css('color','white');
+    // Con jquiery los eventos son mas faciles de capturar
+    const caja = $('#caja');
 
-    // 2. Tambien podemos encontrar un elemento dentro de otro (en caso de no saber donde se encuentra), esto con el metodo find
-    $('p').find('.span')// Enviando como parametro el selector del elemento a buscar - El metodo find, sirve para buscar dentro de un arbol de elementos muy extenso
-          .css('background','green');// En este caso encadenamos un css para cambiar el fondo del elemento encontrado
-    // Se pudo hacer $('p .span') y de igual forma se encuentra los elementos
+    // En este caso aplicamos el evento mouseover - Para esto aplicamos el metodo mouseover y le damos como parametro una funcion de calback en al que ejecutaremos el codigo al capturar el evento
+    caja.mouseover(function () { 
+        $(this).css('background','gray')// En este caso cambiamos el fondo y el texto
+               .text(':(');
+    });
 
-    // 3. Podemos seleccionar   el padre de un elemento usando el metodo "parent()", este metodo da como resultado el elemento HTML padre del elemento actualmente seleccionado
-    var span = $('p .span');// Ojo: Esto no cambia el contenido de una variable, que contenga un elemento html
-    console.log(span.text());// Salida de prueba, el elemento seleccinado sigue siendo el span dentro del parrafo
-    span.parent().css('color','yellow');// Al padre del span (osea el parrafo), le cambiamos el CSS
+    // Lo mismo poara el evento mouseout
+    caja.mouseout(function () { 
+        $(this).css('background','salmon')
+               .text(':)');
+    });
 
-    // 4. Eq() - Buscar un elemento en concreto dentro de una conjunto de elementos HTML, retornado por una seleccion
-    var lista = $('li');// En este caso seleccionamos todos los elemetos Li
-    lista.eq(0).css('background','blue');// y al primero de ellos (el 0), le cambiamos el bg a azul. Seleccionandolo con EQ()
 });
