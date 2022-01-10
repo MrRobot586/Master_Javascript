9bd77a8955f0d1742311fbf91c4e0813b9a4ea03
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-10 09:32:38 -0400

    Jquery dom y textos - JS

--- main.js
+++ main.js
@@ -1,39 +1,57 @@
 /*
-    Jquery en JavaScript | Eventos mousedown y mouseup, mas ejemplos...
+    Jquery en JavaScript | Dom y textos 
+        En este caso veremos un pequeño ejercicio, donde destacaremos la potencia de jquery
 */
-
 
 'use strict'
 
 $(document).ready(function (){
+    /*
+        En el html hay una lista de enlaces y un formulario para añadir nuevos elementos.
+        Con lo cual debemos hacer que esta "lista interactiva", funcione...
+            - La lista esta hecha de enlaces que no tienen un "texto"
+            - El boton para añadir nuevos elances esta deshabilitado
+    */
 
-    // Capturamos el elemento
-    const caja = $('#caja');
+    // En este caso se hizo una funcion que recarga los links y les pone de nombre el propio enlace
+    reload_links();// Invocacion de la funcion
 
-    // Con mousedow, se captura el evento de cuando el click del mouse este precionado
-    caja.mousedown(function (){ 
-        $(this).css('background','gray')
-               .text(':(');
+    const addlink_form = $('#add_link_form');// Usando Jquery obtenemos el elemento form del HTML por su ID
+    
+    // Tambien obtenemos el boton del formulario, esta vez por su atributo "value"
+    $('[value="Añadir"]').removeAttr('disabled');// Y quitamos el atributo disabled, para activarlo y que se pueda precionar
+
+    // Usando el metodo .submit detectamos el evento "submit" en el formulario
+    addlink_form.submit(function (e) {// En este caso recibimos el evento
+        
+        e.preventDefault();// y cancelamos el comportamiento por default de este (que es recargar la pagina)
+
+        const link = $('[name="add_link"]');// Obtenemos el input donde se escribe el link | Por su atributo "name"
+
+        const link_list = $('#links');// Y tambien la lista de links, esta ves por su ID
+        
+        if(link.val() != ''){// Comprobamos que el link no este vacio y armamos un item para la lista con un enlace que contenga ese link
+            const li = document.createElement('li');// Creamos un LI
+            let a = document.createElement('a');// Luego un A
+            a.href = link.val();// A el elace le añadimos el atributo Href y como valor le ponemos el link del input
+            li.append(a);// Le asignamos como hijo el enlace al item para la lista
+            link_list.append(li);// por ultimo, a la lista le asigamos como hijo el item
+            link.val('');// y le quitamos el valor al input de donde se saco el link
+            reload_links();// Ejecutamos la funcion para colocar de nombre los links de los enlaces
+        }
+
     });
-
-    // Y con moseup, capturamos el evento de cuando el mouse deje de estar precionado
-    caja.mouseup(function(){
-        $(this).css('background','salmon')
-               .text(':)');
-    });
-
-    // Nota: En este caso cambiamos los estilos css del elemento seleccionado para cada caso (mousedown y mouseup)
-
-    // Con mousemove(), podemos detectar si es que el mouse se mueve por ensima de un elemento
-    const point = $('#circulo');
-
-    $(document).mousemove(function (event){// En este caso detectamos el movimiento del mouse en todo el documento
-        point.css('left',event.clientX)// Y movemos el elemento a los ejes X y Y en donde se encuentra
-             .css('top',event.clientY);// En cadenamos otro metodo
-             
-        $('body').css('cursor','none');// Tambien quitamos el cursor para apreciar mejor el evento
-    });
-
-    // Nota: con "event.clientXY" obtenemos las coordenadas del la posicion actual del mouse
     
 });
+
+// Funcion para darle nombre a cada enlace - El nombre sera su propio link
+function reload_links() {
+    const a = $('a');// Obtenemos todos los enlaces
+    
+    a.each(function(){// Con el metodo each(), recorremos todos los A capturados con jquery
+        // Creamos una valirable para almacenar el link de los enlaces
+        let href = $(this).attr('href');// Lo igualamos a el valor del atributo del elemento (haciendo referencia con this)
+        $(this).attr('target','blank');// Con el mismo metodo attr(), se pueden poner nuevos atributos y tambien cambiar los ya existentes, ademas de obtener su valor
+        $(this).text(href);// Por ultimo al propio elemento (en el que se esta iterando), se le añade como texto el enlace
+    });
+}
