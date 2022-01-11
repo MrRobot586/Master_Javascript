281689dff9cb3e40308f2bee3e81c083afaca3e1
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-11 08:06:32 -0400

    Animaciones personalizadas - JS

--- main.js
+++ main.js
@@ -1,50 +1,27 @@
 /*
-    Jquery en JavaScript | Efectos y animaciones
-        Con jquery se pueden hacer efectos o animaciones para ocultar y mostrar elementos
-        esto con metodos aplicables  aelementos HTML y cuya animacion puede ser lenta, normal o rapida, dependiendo del
-        parametro que coloquemos en el metodo. 
+    Jquery en JavaScript | Animaciones personalizadas
+         Mediante el metodo "animate()" se pueden crear animaciones personalizadas dando como parametros un json
+         con los parametros css que tendra el elemento al final de la animacion y como segundo parametro la velocidad de esta
+         cabe destacar, que tambien se pueden encadenar varios metodos .animate en un mismo elemento, como una promesa.
 */
 
 'use strict'
 
 $(document).ready(function (){
     
-    const caja = $('#caja');
+    const caja = $('#caja');// Elemento a animar
 
-    const bnt_toggle = $('#toggle_btn');
-    const btn_mostrar = $('#btn_mostrar');
-    const btn_ocultar = $('#btn_ocultar');
+    const btn_animate = $('#animacion');// Boton que activara la animacion
 
-    /*
-        - Hide y show: Ocultar y mostrar un elemento HTML seleccionado. Pero si no espesificamos un parametro solo lo desaparece y aparece, 
-          sin embargo con un parametro de velocidad sea numerico o un string se modifica la velocidad de la animacion, ejemplo con el parametro 'fast'
-          la animacion ira rapido.
-        - Fadein y Fadeout: Hace un fundido al mostrar y ocultar el elemento, fadein es para mostra y fadeout para ocultar.
-        - Fadeto: Hace lo mismo que fadein y fadeout, solo que recibe 2 parametros: La velocidad y el estado final que tendra la animacion.
-          Es decir, en que opacidad quedara el elemento al terminar la animacion (0 para ocuotar y 1 para mostra).
-        - SlideUp y SlideDown: Barren el elemento hacia arriba o abajo al ocultar y mostrar.
-        - Toggle: Con solo esta funcion se pueden mostrar y ocultar elementos (es como hide y show) y tiene variaciones para los demas metodos
-            - FadeToggle
-            - SlideToggle
-    */
-
-    btn_mostrar.click(function(){
-        //caja.show('normal');
-        //caja.fadeIn('normal');
-        //caja.fadeTo('normal', 1);
-        caja.slideDown();
-    });
-
-    btn_ocultar.click(function(){
-        //caja.hide('fast');
-        //caja.fadeOut('low');
-        //caja.fadeTo('normal', 0);
-        caja.slideUp();
-    });
-
-    bnt_toggle.click(function(){
-        //caja.toggle();
-        //caja.fadeToggle();
-        caja.slideToggle();
+    btn_animate.click(function (){// EN el evento click
+        caja.animate({// APlicamos el metodo animate al elemento
+            marginLeft: '500px'// Al finald de la animacion tendra este margin
+        }, 'normal')// y la animacion se hara en velocidada normal
+        .animate({// Como se ve tambien se encadena otra animacion con el mismo formato esta se ejecutara cuando la anterior termine
+            marginLeft: '0px'
+        }, 'slow')
+        .animate({
+            marginLeft: '500px'
+        }, 'fast');
     });
 });
