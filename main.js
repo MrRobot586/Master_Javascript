381f54012e9a38dd1ec8d9e2883a77465ec91e62
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-11 07:36:48 -0400

    Animaciones en JQuery - JS

--- main.js
+++ main.js
@@ -1,57 +1,50 @@
 /*
-    Jquery en JavaScript | Dom y textos 
-        En este caso veremos un pequeño ejercicio, donde destacaremos la potencia de jquery
+    Jquery en JavaScript | Efectos y animaciones
+        Con jquery se pueden hacer efectos o animaciones para ocultar y mostrar elementos
+        esto con metodos aplicables  aelementos HTML y cuya animacion puede ser lenta, normal o rapida, dependiendo del
+        parametro que coloquemos en el metodo. 
 */
 
 'use strict'
 
 $(document).ready(function (){
+    
+    const caja = $('#caja');
+
+    const bnt_toggle = $('#toggle_btn');
+    const btn_mostrar = $('#btn_mostrar');
+    const btn_ocultar = $('#btn_ocultar');
+
     /*
-        En el html hay una lista de enlaces y un formulario para añadir nuevos elementos.
-        Con lo cual debemos hacer que esta "lista interactiva", funcione...
-            - La lista esta hecha de enlaces que no tienen un "texto"
-            - El boton para añadir nuevos elances esta deshabilitado
+        - Hide y show: Ocultar y mostrar un elemento HTML seleccionado. Pero si no espesificamos un parametro solo lo desaparece y aparece, 
+          sin embargo con un parametro de velocidad sea numerico o un string se modifica la velocidad de la animacion, ejemplo con el parametro 'fast'
+          la animacion ira rapido.
+        - Fadein y Fadeout: Hace un fundido al mostrar y ocultar el elemento, fadein es para mostra y fadeout para ocultar.
+        - Fadeto: Hace lo mismo que fadein y fadeout, solo que recibe 2 parametros: La velocidad y el estado final que tendra la animacion.
+          Es decir, en que opacidad quedara el elemento al terminar la animacion (0 para ocuotar y 1 para mostra).
+        - SlideUp y SlideDown: Barren el elemento hacia arriba o abajo al ocultar y mostrar.
+        - Toggle: Con solo esta funcion se pueden mostrar y ocultar elementos (es como hide y show) y tiene variaciones para los demas metodos
+            - FadeToggle
+            - SlideToggle
     */
 
-    // En este caso se hizo una funcion que recarga los links y les pone de nombre el propio enlace
-    reload_links();// Invocacion de la funcion
-
-    const addlink_form = $('#add_link_form');// Usando Jquery obtenemos el elemento form del HTML por su ID
-    
-    // Tambien obtenemos el boton del formulario, esta vez por su atributo "value"
-    $('[value="Añadir"]').removeAttr('disabled');// Y quitamos el atributo disabled, para activarlo y que se pueda precionar
-
-    // Usando el metodo .submit detectamos el evento "submit" en el formulario
-    addlink_form.submit(function (e) {// En este caso recibimos el evento
-        
-        e.preventDefault();// y cancelamos el comportamiento por default de este (que es recargar la pagina)
-
-        const link = $('[name="add_link"]');// Obtenemos el input donde se escribe el link | Por su atributo "name"
-
-        const link_list = $('#links');// Y tambien la lista de links, esta ves por su ID
-        
-        if(link.val() != ''){// Comprobamos que el link no este vacio y armamos un item para la lista con un enlace que contenga ese link
-            const li = document.createElement('li');// Creamos un LI
-            let a = document.createElement('a');// Luego un A
-            a.href = link.val();// A el elace le añadimos el atributo Href y como valor le ponemos el link del input
-            li.append(a);// Le asignamos como hijo el enlace al item para la lista
-            link_list.append(li);// por ultimo, a la lista le asigamos como hijo el item
-            link.val('');// y le quitamos el valor al input de donde se saco el link
-            reload_links();// Ejecutamos la funcion para colocar de nombre los links de los enlaces
-        }
-
+    btn_mostrar.click(function(){
+        //caja.show('normal');
+        //caja.fadeIn('normal');
+        //caja.fadeTo('normal', 1);
+        caja.slideDown();
     });
-    
+
+    btn_ocultar.click(function(){
+        //caja.hide('fast');
+        //caja.fadeOut('low');
+        //caja.fadeTo('normal', 0);
+        caja.slideUp();
+    });
+
+    bnt_toggle.click(function(){
+        //caja.toggle();
+        //caja.fadeToggle();
+        caja.slideToggle();
+    });
 });
-
-// Funcion para darle nombre a cada enlace - El nombre sera su propio link
-function reload_links() {
-    const a = $('a');// Obtenemos todos los enlaces
-    
-    a.each(function(){// Con el metodo each(), recorremos todos los A capturados con jquery
-        // Creamos una valirable para almacenar el link de los enlaces
-        let href = $(this).attr('href');// Lo igualamos a el valor del atributo del elemento (haciendo referencia con this)
-        $(this).attr('target','blank');// Con el mismo metodo attr(), se pueden poner nuevos atributos y tambien cambiar los ya existentes, ademas de obtener su valor
-        $(this).text(href);// Por ultimo al propio elemento (en el que se esta iterando), se le añade como texto el enlace
-    });
-}
