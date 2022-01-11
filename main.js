b7ebe21ddbc4f10f7c353de82818418eef2ca650
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-11 10:39:11 -0400

    Peticiones ajax: Load jquery - JS

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
 /*
-    Jquery en JavaScript | Animaciones personalizadas
-         Mediante el metodo "animate()" se pueden crear animaciones personalizadas dando como parametros un json
-         con los parametros css que tendra el elemento al final de la animacion y como segundo parametro la velocidad de esta
-         cabe destacar, que tambien se pueden encadenar varios metodos .animate en un mismo elemento, como una promesa.
+    Jquery en JavaScript | Peticiones Ajax
 */
 
 'use strict'
 
 $(document).ready(function (){
     
-    const caja = $('#caja');// Elemento a animar
+    /*
+        Load - permite hacer una peticion ajax por get e inscrustar el resultado en un elemento HTML seleccionado
+               Algunas api rest devuelven como resultado un troso de HTMl con los datos que luego se pueden incrustar en el HTML 
+               de la actual pagina, para eso sirve el metodo ".load()" ya que carga un HTMl devuelto por una URL y lo incrusta en
+               un elmento seleccionado.
+    */
 
-    const btn_animate = $('#animacion');// Boton que activara la animacion
+    $('#load').load('https://reqres.in/');// En este caso en el div con el ID "load" incrustamos la pagina de la URL que se manda como parametro en el metodo load
 
-    btn_animate.click(function (){// EN el evento click
-        caja.animate({// APlicamos el metodo animate al elemento
-            marginLeft: '500px'// Al finald de la animacion tendra este margin
-        }, 'normal')// y la animacion se hara en velocidada normal
-        .animate({// Como se ve tambien se encadena otra animacion con el mismo formato esta se ejecutara cuando la anterior termine
-            marginLeft: '0px'
-        }, 'slow')
-        .animate({
-            marginLeft: '500px'
-        }, 'fast');
-    });
 });
