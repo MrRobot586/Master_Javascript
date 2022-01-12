3d95eb28ef0a700958c8ee411da8b1087ad0b810
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-12 07:44:54 -0400

    Draggable jquery ui - JS

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 /*
     JqueryUI en JavaScript | Plugin para crear interfaces con jquery
-        Integracion: Se descarga el archivo correspondiente a la vercion desada y se enlaza con un script en el HTML
-        hay varios archivos en la carpeta de la libreria, para usar lo basico solo se incluye el ".min.js" 
-
-        Esta trae consigo una serie de metodos y funciones para crear elmentos y efectos en una pagina web, mediante javascript.
 */
 
 'use strict'
 
 $(document).ready(function (){
 
-    
+    // Draggable - Hacer que un elemento se pueda mover al mantenerlo pulsado con el mouse
+    $('.caja').draggable();
 
 });
