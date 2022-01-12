a2e685b5f0101e4015af7b1cecacc3c3d32b4f47
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-12 08:07:46 -0400

    Selectable jquery ui - JS

--- main.js
+++ main.js
@@ -6,8 +6,9 @@
 
 $(document).ready(function (){
 
-    // Resizable - Elementos que se pueden cambiar de tamaño el el DOM
-    // Para esto se incluye el "min.css" de jquery UI en el HTML
-    $('.caja').resizable();
+    // Selectable - Permite seleccionar elementos de una lista dando click ensima 
+    $('.lista').selectable();// Se aplica el metodo a una lista (ul/ol)
+    // Hay que mencionar que se tiene que añadir al css las reglas del formato de los elemntos seleccionados
+    // Para ello se le dan estilos a las clases "ui-selected" y "ul-selecting"
 
 });
