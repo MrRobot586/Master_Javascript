8051fc1b533c37a383e840fb0fbf0ffa1ee70cb2
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-13 07:37:46 -0400

    Datepicker jquery ui - JS

--- main.js
+++ main.js
@@ -6,10 +6,7 @@
 
 $(document).ready(function (){
 
-    // Cuadros de dialogo | dialog()
-    // Sirven como aviso dentro de la pagina frente a algun evento, en este caso el de un click en un boton
-    $('#btn').click(function(){
-        $('[title="¿Estas seguro?"]').dialog();// Aplicando el metodo al elemento, se mostrara un cuadro de dialogo con el contenido de ese lemento
-    });
-    // Nota: El "title" de el elemento al que se aplica "dialog()", sera la cabecera de ese cuadro de dialogo
+    // Date picker - Calendario
+    // Un tipico calendario en el que seleccionas una fecha y esta la pone en el camppo que ibas a llenar
+    $('[name="calendar"]').datepicker();// Este metodo se le aplica a un input y listo
 });
