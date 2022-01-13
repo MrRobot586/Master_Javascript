adda43fdbe7d368bacee67c6815fc2c0be5805b6
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-13 07:25:56 -0400

    Tooltips jquery ui - JS

--- main.js
+++ main.js
@@ -6,26 +6,10 @@
 
 $(document).ready(function (){
 
-    // Efectos en jquery UI
-    $('#activar_efecto').click(function(){
-        $('#caja').toggle('puff');
-
-        /*
-            Con jquery UI se añaden muchos efectos nuevos para ocultar y mostrar elementos:
-                - puff
-                - fade
-                - explode
-                - blind 
-                - slide
-                - drop
-                - fold
-                - scale
-                - shake
-                - 
-            Los cuales se pueden usar con el metodo toggle(efecto) o con effect(efecto)
-            Enviado como parametro el nombre del efecto que queremos aplicar al elemento
-            Nota: Tambien se pueden pasar opciones y la velocidad del efecto, pero son parametros opcionales.
-        */
-    });
+    // Tootips - Pequeño popup que se ve cuando pasamos el mouse por ensima de un elemento
+    // Aplicando el metodo "tooltip()" a un elemento se le coloca un tooltip al elmento
+    $('[title="Ir a google..."]').tooltip();// Se puede aplicar a un elemento
+    //$(document).tooltip();// O a todo el documento
+    //Nota: para que funcione el elemento debe tener un atributo "title"
 
 });
