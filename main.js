41214f62499b6c86c44f2386ef71f7db91442d9b
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-13 07:33:15 -0400

    Dialogos jquery ui - JS

--- main.js
+++ main.js
@@ -6,10 +6,10 @@
 
 $(document).ready(function (){
 
-    // Tootips - Pequeño popup que se ve cuando pasamos el mouse por ensima de un elemento
-    // Aplicando el metodo "tooltip()" a un elemento se le coloca un tooltip al elmento
-    $('[title="Ir a google..."]').tooltip();// Se puede aplicar a un elemento
-    //$(document).tooltip();// O a todo el documento
-    //Nota: para que funcione el elemento debe tener un atributo "title"
-
+    // Cuadros de dialogo | dialog()
+    // Sirven como aviso dentro de la pagina frente a algun evento, en este caso el de un click en un boton
+    $('#btn').click(function(){
+        $('[title="¿Estas seguro?"]').dialog();// Aplicando el metodo al elemento, se mostrara un cuadro de dialogo con el contenido de ese lemento
+    });
+    // Nota: El "title" de el elemento al que se aplica "dialog()", sera la cabecera de ese cuadro de dialogo
 });
