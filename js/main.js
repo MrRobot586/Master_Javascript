1e6e2bedee837332c0a658088213cfb74a351d55
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-18 08:50:54 -0400

    Proyecto JS parte 1 - Maquetacion HTML y CSS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*
+    Proyecto completo con JS y Jquery
+        Este proyecto sera una practica para usar todos los conocimientos adquiridos en el curso
+*/
+
+'use strict'
+
+$(document).ready(function (){
+
+    
+
+});
