39c1807acebdace4399867ed9e8c555cecf4f426
Diego Oropeza <diegojoseoropezagmail.com>
2022-01-21 16:12:21 -0400

    Proyecto JS parte 2 - Slider con plugin jquery

--- js/main.js
+++ js/main.js
@@ -7,6 +7,16 @@
 
 $(document).ready(function (){
 
-    
+    // Slider show | Usando el plugin bxslider
+    $('#slidershow').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1200
+    });
+
+    /*
+        Es muy facil, simplemente se incluyen los archivos de la libreria y se aplica el metodo al elemento.
+        En este caso, pasamos como parametro un json que tiene las opciones del slider, como su animacion, etc...
+    */
 
 });
