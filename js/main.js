7b9ae1da8ee8ffae5e0db703f694e3ef1633ae7f
Diego Oropeza <diegojoseoropezagmail.com>
2022-02-02 21:09:22 -0400

    POO 2 - Metodos y Metodo constructor

--- js/main.js
+++ js/main.js
@@ -1,42 +1,30 @@
 /*
   Programacion Orientada a Objetos en TypeScript
-    Este lenguaje tiene una version mas robusta de la programacion orientada a objetos que la que provee JavaScript,
-    ademas que al transpilarse a javascript convierte el codigo a compatible con todos los navegadores, incluyendo navegadores
-    antiguos.
+    Metodo Constructor: Este como su nombre lo indica, construye el objeto al instanciarlo o al crearlo.
+    Este metodo se encarga de asignarle un valor a las propiedades del objeto por medio de la instancia,
+    es decir, al instanciar un objeto nuevo de una clase, como parametro se pasan los valores que se asignaran a
+    las propiedades de este y el metodo constructor se ejecuta para asignar los valores. Ejemplo:
 
-    P.O.O: COmo se usa en Ts no es aplicable a JS nativo.
+      var objeto = new verga(Tamaño, grosor);
 
-    Un objeto es una entidad con propiedades y metodos, que se asemeja a un objeto de la vida real. Un objeto se crea a partir de una clase.
-    Los objetos pueden tener todo tipo de propiedades que pueden ser publicas, privadas o protegidas, dependiendo de como sean
-    podran editarse o no fuera del objeto o solo mediante un metodo.
-    Los metodos pueden obtener datos de las propiedades o setearlos (osea obtener su valor o cambiarlo). Si una propiedad es privada
-    solo se puede cambiar su valor mediante un metodo del objeto, si es publica se podra setear haciendo referencia al objeto y propiedad.
-    Los metodos y propiedades se definene en la clase a partir de la cual se crea un objeto.
-    Se pueden crear muchos objetos a partir de una misma clase.
-    Un objeto se crea instanciando una clase en una variable.
-      Ejemplo:var objeto = new Camiseta();
-
-  Clases en TS:
-    Molde para crear objetos que compartan ciertas caracteristicas, ejemeplo: un molde para crear una bicicleta
-    En una clase se define las propiedades del objeto y sus metodos.
-      - Las propiedades: Son valores guardados en variables. Ejemplo: Una bicicleta tiene marca, esa seria una caracteristica o propiedad.
-      - Los metodos: Son acciones o funciones que realiza el objeto en cuestion. Ejemplo: Una bicicleta rueda, ese seria un metodo.
-
-    Para definir una clase se usa la palabra reservada class, seguido del nombre de la clase y llaves. El nombre de la clase
-    debe tener la primera letra en mayuscula.
-
-    Para definir una propiedad, se coloca la visibilidad de la propiedad, seguido del nombre,":" y el tipado, se cierra con ";"
-      Una propiedad puede ser publica (public), privada (private) o protegida (protected). Dependiendo de esto la propiedad podra o no ser
-      editada fuera del objeto.
-
-    Para definir un metodo,
-
-    Es recomendable definir la clase en un fichero a parte y que este tenga el nombre de la clase como tal.
+    El metodo constructor se define en la clase con la palabra reservada "constructor", este nunca devuelve ningun valor.
 */
 var Camiseta = /** @class */ (function () {
-    function Camiseta() {
+    // Metodo constructor: En este caso el constructor recibe un objeto JSON para los datos que se asignaran al nuevo objeto, pero bien podria recibir parametros individuales
+    function Camiseta(data) {
+        // Es te metodo no hace mas que asignar lo que obtiene como parametro como valor a las propiedades de un neuvo objeto creado con esta clase
+        this.color = data.color; // Se hace referencia al mismo objeto con el operador "this"
+        this.modelo = data.modelo;
+        this.marca = data.marca;
+        this.talla = data.talla;
+        // Nota: Tambien se podrian hacer comprobaciones de los parametros para poder asignar los valores como si es un numero y cosas asi
+        if (!isNaN(data.precio)) { // Como aca que se comprueba si es un numero el parametro
+            this.precio = data.precio;
+        }
+        else {
+            this.precio = 0;
+        }
     }
-    // Metodos:
     Camiseta.prototype.setColor = function (color) {
         this.color = color;
     };
@@ -45,8 +33,13 @@ var Camiseta = /** @class */ (function () {
     };
     return Camiseta;
 }());
+var datos = {
+    color: "Rojo",
+    modelo: "Manga larga",
+    marca: "Nike",
+    talla: "L",
+    precio: 12.5
+};
 // Instanciar un nuevo objeto camiseta:
-var camisa = new Camiseta();
-// Usar el metodo setColor() para cambiar el color al nuevo objeto
-camisa.setColor("Rojo");
-console.log(camisa.getColor()); // Usar el metodo getColor() para obtener el color de la camisa, anteriormente e
+var camisa = new Camiseta(datos);
+console.log(camisa);
